18870bfcbb7c116aded734e56714c226462a5e8d
Israel Saeta Pérez <dukebody@gmail.com>
2010-03-28 16:35:21 +0200

    Add price to the sql schema.

--- db_schema.sql
+++ db_schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE items
 (id integer not null primary key,
  title char(100) not null,
  description varchar(500),
+ price decimal(2) not null,
  date timestamp not null, 
  buy_sell boolean not null,
  hash char(8) not null
