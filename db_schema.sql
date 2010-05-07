2c0ccfee19900b81189f67686502f5607ea919f1
Israel Saeta Pérez <dukebody@gmail.com>
2010-05-07 15:53:11 +0200

    Update db schema.

--- db_schema.sql
+++ db_schema.sql
@@ -1,9 +1,11 @@
 CREATE TABLE items
 (id integer not null primary key,
- title char(100) not null,
+ name char(100) not null,
  description varchar(500),
  price decimal(2) not null,
  date timestamp not null, 
+ location char(100),
+ contact char(100) not null,
  buy_sell boolean not null,
  hash char(8) not null
 );
